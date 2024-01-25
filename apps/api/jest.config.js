24986137a2190796a03460b053ba9b611943fb56
markitos-info <150465953+markitos-info@users.noreply.github.com>
2024-01-25 10:49:11 +0100

    Update jest.config.js

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -1,6 +1,20 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testResultsProcessor: "jest-sonar-reporter",
+    preset: 'ts-jest',
+    reporters: ['default',  'jest-sonar'],
+    testEnvironment: 'node',
+    cacheDirectory: '/tmp/jestCache',
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
+        '!<rootDir>/src/**/*.d.ts',
+        '!<rootDir>/src/**/*.test.{js,jsx,ts,tsx}'
+    ],
+    coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '/src/index.ts',
+        '/src/index.tsx'
+    ],
+    testMatch: [
+        '<rootDir>/test/**/*.test.{js,jsx,ts,tsx}'
+    ]
 };
